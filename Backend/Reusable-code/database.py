7de0d583a9c012fea7683d4613c4f22649e88f96
zZHugoZz <123295904+zZHugoZz@users.noreply.github.com>
2023-07-19 19:04:52 +0200

    Update database.py
    Added the function to get the database

--- Backend/Reusable-code/database.py
+++ Backend/Reusable-code/database.py
@@ -8,3 +8,10 @@ SQLALCHEMY_DATABASE_URL = "<database>://<user_name>:<password>@<ip_address>/<dat
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

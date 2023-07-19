072ed9110212507c171731b4403910b3a87bece7
zZHugoZz <123295904+zZHugoZz@users.noreply.github.com>
2023-07-19 19:20:56 +0200

    Update main.py
    deleted the try/except block as it wasn't needed

--- Backend/Reusable-code/main.py
+++ Backend/Reusable-code/main.py
@@ -11,23 +11,6 @@ from .database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-while True:
-    """creates a connection to the database"""
-    try:
-        conn = psycopg2.connect(
-            host="<host>",
-            database="<database>",
-            user="<user>",
-            password="<password>",
-            cursor_factory=RealDictCursor
-        )
-        cursor = conn.cursor()
-        print("\033[1;32;40mdatabase connection was successful!")
-        break
-    except Exception as e:
-        print(f"\033[1;31;40merror: {e}")
-        time.sleep(2)
-
 @app.get("/test")
 def test_request(db: Session = Depends(get_db)):
     """This is a test function to execute a basic query, the database will return

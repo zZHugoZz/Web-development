7afbb2f1a3d094c5cdb6305a493d0bd77708ab9b
zZHugoZz <123295904+zZHugoZz@users.noreply.github.com>
2023-07-20 13:39:39 +0200

    Update schemas.py
    added the model_config

--- Backend/Reusable-code/schemas.py
+++ Backend/Reusable-code/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 
 class Product(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
     name: str
     price: float
     description: str | None = None
